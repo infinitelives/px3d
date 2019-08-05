e725c709fc80e09156c3d31531d989d9b103508c
Chris McCormick <chris@mccormick.cx>
2019-08-05 21:06:00 +0800

    Good ground + fog combination. Models from hash.

--- src/px3d/core.cljs
+++ src/px3d/core.cljs
@@ -7,6 +7,9 @@
 (def scene js/scene)
 (def THREE js/THREE)
 
+;(def background-color 0x20AAF3)
+(def background-color 0xffffff)
+
 (def loader (THREE.GLTFLoader.))
 
 ;; -------------------------
@@ -43,26 +46,42 @@
            ; This will add the entire Blender scene into the threejs scene
            ;(.add scene (.-scene gltf))
 
-           (doseq [x (range 10)]
-             (let [tree (.clone (aget gltf "scene" "children" 40))]
-               (-> tree .-position (.set (* 20 (- (js/Math.random) 0.5)) 0 (* 20 (- (js/Math.random) 0.5))))
-               (.add scene tree)))
+           ; add a ground plane
+           (let [ground (THREE.Mesh.
+                          (THREE.CylinderGeometry. 150 150 1 32)
+                          (THREE.MeshLambertMaterial. #js {:color 0x637C60 :specular 0x000000 :shininess 0}))]
+             (aset ground "position" "y" -0.5)
+             (aset ground "receiveShadow" true)
+             (aset ground "castShadow" true)
+             (.add scene ground))
 
-           ;(aset gltf "scene" "children" 2 "position" "x" 5)
+           ; add fog
+           (aset scene "fog" (THREE.FogExp2. background-color 0.0128 10))
+           (aset scene "background" (THREE.Color. background-color))
 
-           (.add scene (aget gltf "scene" "children" 41))
+           (let [models (make-models-hash gltf)]
+             (doseq [x (range 100)]
+               (let [tree (.clone (get models (choice ["Tree" "Tree2"])))]
+                 (-> tree .-position (.set (* 150 (- (js/Math.random) 0.5)) 0 (* 150 (- (js/Math.random) 0.5))))
+                 (aset tree "rotation" "y" (* (js/Math.random) js/Math.PI 2))
+                 (aset tree "scale" "y" (+ (* (js/Math.random) 0.2) 1))
+                 (.add scene tree)))
 
-           ; parent the player object from Blender to an empty "player"
-           ; Mesh so that we can position it and animate the Blender object
-           (let [player (THREE.Mesh.)
-                 chr (.clone (aget gltf "scene" "children" 2))
-                 mixer (THREE.AnimationMixer. scene)
-                 clip (THREE.AnimationClip.findByName (aget gltf "animations") "Bob")]
-             (-> player .-position (.set 10 0 10))
-             (.add player chr)
-             (.add scene player)
-             (.play (.clipAction mixer clip chr))
-             (aset js/window "mixer" mixer)))))
+             ;(aset gltf "scene" "children" 2 "position" "x" 5)
+
+             ;(.add scene (aget gltf "scene" "children" 41))
+
+             ; parent the player object from Blender to an empty "player"
+             ; Mesh so that we can position it and animate the Blender object
+             (let [player (THREE.Mesh.)
+                   chr (.clone (get models "Character"))
+                   mixer (THREE.AnimationMixer. scene)
+                   clip (THREE.AnimationClip.findByName (aget gltf "animations") "Bob")]
+               (-> player .-position (.set 10 0 10))
+               (.add player chr)
+               (.add scene player)
+               (.play (.clipAction mixer clip chr))
+               (aset js/window "mixer" mixer))))))
 
 (defn mount-root []
   ;(r/render [home-page] (.getElementById js/document "app"))
